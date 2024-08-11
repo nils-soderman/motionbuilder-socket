79decd2aedeeb658ed48b9ae0a10b3a4097f33c9
Nils Söderman <rymdnisse@live.com>
2024-08-11 14:32:28 +0200

    test_golbals: Python 2 support

--- src/__tests__/fixtures/test_globals.py
+++ src/__tests__/fixtures/test_globals.py
@@ -4,7 +4,7 @@ This test file is used in the 'Exec Globals' test case.
 
 def test_global(key, expected_value):
     value = globals().get(key)
-    assert value == expected_value, f"{key} is incorrect, expected {expected_value}, got {value}"
+    assert value == expected_value, "%s is incorrect, expected %s, got %s" % (key, expected_value, value)
 
 test_global("test_boolean", True)
 test_global("test_none", None)
